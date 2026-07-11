6b0719523b65ae57eb62794db95fc572bac43f00
Murate-Lab <dev@murate.jp>
2026-07-11 20:50:35 +0900

    文字化け修正

--- WinTachos/WinTachos.h
+++ WinTachos/WinTachos.h
@@ -12,55 +12,55 @@
 #include "pdhmsg.h"
 #pragma comment(lib, "pdh.lib")
 
-#define VERSION					2.00				// �o�[�W����
+#define VERSION					2.00				// バージョン
 #define COLORKEY					RGB(1, 0, 1)			// Layered window transparency key
 
 #define MAX_LOADSTRING 100
-#define TIMERID					1					// WM_PAINT���s�̂��߂̃^�C�}�[ID
-#define	MM_TRAY					(WM_USER + 0)		// �^�X�N�g���C�̃A�C�R���N���b�N��
-#define LOGMAX					200					// �L�^���
+#define TIMERID					1					// WM_PAINT発行のためのタイマーID
+#define	MM_TRAY					(WM_USER + 0)		// タスクトレイのアイコンクリック時
+#define LOGMAX					200					// 記録上限
 
 #ifdef _PS13
-	#define WINDOWSZ_X			600					// �T�C�YHuge���̃E�B���h�E�T�C�Y�w
-	#define WINDOWSZ_Y			200					// �T�C�YHuge���̃E�B���h�E�T�C�Y�x
-	#define SPEEDM_CENTER_X		147					// �X�s�[�h���[�^�[�̒��S���W�w
-	#define SPEEDM_CENTER_Y		150					// �X�s�[�h���[�^�[�̒��S���W�x
-	#define SPEEDM_CENTER_R		12					// �X�s�[�h���[�^�[�̒��S���̔��a
-	#define SPEEDM_LENG			131					// �X�s�[�h���[�^�[�̐j�̒���
-	#define SPEEDM_LENG_B		22					// �X�s�[�h���[�^�[�̐j�̒����i���Α��j
-	#define SPEED_MIN_R			198.4f				// �X�s�[�h���[�^�[��0km�̈ʒu�̊p�x
-	#define SPEED_MAX_R			-18.6f				// �X�s�[�h���[�^�[��180km�̈ʒu�̊p�x
-	#define TACHOM_CENTER_X		452					// �^�R���[�^�[�̒��S���W�w
-	#define TACHOM_CENTER_Y		150					// �^�R���[�^�[�̒��S���W�x
-	#define TACHOM_CENTER_R		12					// �^�R���[�^�[�̒��S���̔��a
-	#define TACHOM_LENG			131					// �^�R���[�^�[�̐j�̒���
-	#define TACHOM_LENG_B		22					// �^�R���[�^�[�̐j�̒����i���Α��j
-	#define TACHO_MIN_R			199.1f				// �^�R���[�^�[��0rpm�̈ʒu�̊p�x
-	#define TACHO_MAX_R			-18.0f				// �^�R���[�^�[��9000rpm�̈ʒu�̊p�x
-	#define NEEDLE_COLOR		RGB(255,255,255)	// ���[�^�[�̐j�̐F
+	#define WINDOWSZ_X			600					// サイズHuge時のウィンドウサイズＸ
+	#define WINDOWSZ_Y			200					// サイズHuge時のウィンドウサイズＹ
+	#define SPEEDM_CENTER_X		147					// スピードメーターの中心座標Ｘ
+	#define SPEEDM_CENTER_Y		150					// スピードメーターの中心座標Ｙ
+	#define SPEEDM_CENTER_R		12					// スピードメーターの中心軸の半径
+	#define SPEEDM_LENG			131					// スピードメーターの針の長さ
+	#define SPEEDM_LENG_B		22					// スピードメーターの針の長さ（反対側）
+	#define SPEED_MIN_R			198.4f				// スピードメーターの0kmの位置の角度
+	#define SPEED_MAX_R			-18.6f				// スピードメーターの180kmの位置の角度
+	#define TACHOM_CENTER_X		452					// タコメーターの中心座標Ｘ
+	#define TACHOM_CENTER_Y		150					// タコメーターの中心座標Ｙ
+	#define TACHOM_CENTER_R		12					// タコメーターの中心軸の半径
+	#define TACHOM_LENG			131					// タコメーターの針の長さ
+	#define TACHOM_LENG_B		22					// タコメーターの針の長さ（反対側）
+	#define TACHO_MIN_R			199.1f				// タコメーターの0rpmの位置の角度
+	#define TACHO_MAX_R			-18.0f				// タコメーターの9000rpmの位置の角度
+	#define NEEDLE_COLOR		RGB(255,255,255)	// メーターの針の色
 #endif
 
-class		CWTSetting {					// �ݒ���N���X
+class		CWTSetting {					// 設定情報クラス
 public:
-	DWORD		dwWindowPosX;				// �E�B���h�E�̈ʒu�w
-	DWORD		dwWindowPosY;				// �E�B���h�E�̈ʒu�x
-	BOOL		bTopmost;					// ��Ɏ�O�ɕ\��
-	UINT		uiSize;						// �T�C�Y 0:Large 1:Normal 2:Small
-	UINT		uiTimerElapse;				// �^�C�}�[�Ԋu
-	UINT		uiResponse;					// ���X�|���X
+	DWORD		dwWindowPosX;				// ウィンドウの位置Ｘ
+	DWORD		dwWindowPosY;				// ウィンドウの位置Ｙ
+	BOOL		bTopmost;					// 常に手前に表示
+	UINT		uiSize;						// サイズ 0:Large 1:Normal 2:Small
+	UINT		uiTimerElapse;				// タイマー間隔
+	UINT		uiResponse;					// レスポンス
 }			m_SettingInfo;
 
-class		CWTNeedle {						// �j���N���X
+class		CWTNeedle {						// 針情報クラス
 public:
-	POINT		poCenter;					// ���S���W
-	int			uiCenterR;					// ���S���̔��a
-	int			uiLeng;						// �j�̒���
-	int			uiLengB;					// �j�̒����i���Α��j
-	float		fMinR;						// 0km,0rpm�̈ʒu�̊p�x
-	float		fMaxR;						// 180km,9000rpm�̈ʒu�̊p�x
+	POINT		poCenter;					// 中心座標
+	int			uiCenterR;					// 中心軸の半径
+	int			uiLeng;						// 針の長さ
+	int			uiLengB;					// 針の長さ（反対側）
+	float		fMinR;						// 0km,0rpmの位置の角度
+	float		fMaxR;						// 180km,9000rpmの位置の角度
 }			*m_NeedleInfo;
 
-// ���̃R�[�h ���W���[���Ɋ܂܂��֐��̑O�錾:
+// このコード モジュールに含まれる関数の前宣言:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
